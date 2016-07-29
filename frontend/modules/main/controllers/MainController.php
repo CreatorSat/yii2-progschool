2bb0cdff52722aaf147b1162793f2c2fdec9e494
Manin <manin@kbelektron.ru>
2016-07-29 19:18:51 +0300

    lesson 3 - part 1

--- frontend/modules/main/controllers/MainController.php
+++ frontend/modules/main/controllers/MainController.php
@@ -2,10 +2,15 @@
 namespace app\modules\main\controllers;
 
 use frontend\models\Image;
+use frontend\models\SignupForm;
 use yii\web\Controller;
+use yii\web\Response;
+use yii\widgets\ActiveForm;
 
 class MainController extends Controller
 {
+    public $layout = "inner";
+
     public function actionIndex()
     {
         $image_url = Image::getImageUrl();
@@ -13,4 +18,20 @@ class MainController extends Controller
         return $this->render('index');
     }
 
+    public function actionRegister()
+    {
+        $model = new SignupForm;
+
+        if (\Yii::$app->request->isAjax && \Yii::$app->request->isPost) {
+            \Yii::$app->response->format = Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
+        }
+
+        if ($model->load(\Yii::$app->request->post()) && $model->signup()) {
+            print_r($model->getAttributes());
+            die;
+        }
+
+        return $this->render("register", ['model' => $model]);
+    }
 }
---
+++ frontend/modules/main/views/main/register.php
@@ -0,0 +1,19 @@
+<div class="row register">
+    <div class="col-lg-6 col-lg-offset-3 col-sm-6 col-sm-offset-3 col-xs-12 ">
+
+        <? $form = \yii\bootstrap\ActiveForm::begin([
+            'enableClientValidation' => false,
+            'enableAjaxValidation' => false,
+        ]); ?>
+
+        <?= $form->field($model, 'username'); ?>
+        <?= $form->field($model, 'email'); ?>
+        <?= $form->field($model, 'password')->passwordInput(); ?>
+        <?= $form->field($model, 'repassword')->passwordInput(); ?>
+
+        <? echo \yii\helpers\Html::submitButton('Register', ['class' => 'btn btn-success']); ?>
+
+        <? \yii\bootstrap\ActiveForm::end(); ?>
+    </div>
+
+</div>
